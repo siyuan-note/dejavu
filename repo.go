d3b4a6d564425e8b7c476891fa37fb7c06e66f6c
Liang Ding <d@b3log.org>
2022-06-15 00:31:49 +0800

    :art: Chunk

--- repo.go
+++ repo.go
@@ -36,17 +36,15 @@ type Repo struct {
 	Path     string // 仓库的绝对路径，如：F:\\SiYuan\\history\\
 	store    *Store // 仓库的存储
 
-	ChunkPol   chunker.Pol      // 文件分块多项式值
-	bufferPool *util.BufferPool // 文件分块数据缓冲池
+	ChunkPol chunker.Pol // 文件分块多项式值
 }
 
 // NewRepo 创建一个新的仓库。
 func NewRepo(dataPath, repoPath string, aesKey []byte) (ret *Repo, err error) {
 	ret = &Repo{
-		DataPath:   filepath.Clean(dataPath),
-		Path:       filepath.Clean(repoPath),
-		ChunkPol:   chunker.Pol(0x3DA3358B4DC173), // TODO：固定多项式值副作用
-		bufferPool: util.NewBufferPool(4, chunker.MaxSize),
+		DataPath: filepath.Clean(dataPath),
+		Path:     filepath.Clean(repoPath),
+		ChunkPol: chunker.Pol(0x3DA3358B4DC173), // 固定多项式值
 	}
 	ret.DataPath = filepath.Clean(ret.DataPath)
 	if !strings.HasSuffix(ret.DataPath, string(os.PathSeparator)) {
