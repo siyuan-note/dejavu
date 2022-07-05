b9c452dfa0ba4a4349ace14a57e94e2baa197a91
Liang Ding <d@b3log.org>
2022-07-05 17:04:34 +0800

    :sparkles: 数据仓库支持云端备份 https://github.com/siyuan-note/siyuan/issues/5336

--- backup.go
+++ backup.go
@@ -19,15 +19,106 @@ import (
 	"fmt"
 
 	"github.com/88250/gulu"
+	"github.com/siyuan-note/dejavu/entity"
 	"github.com/siyuan-note/httpclient"
 )
 
-func GetCloudRepoRefsFiles(repo string, cloudInfo *CloudInfo) (files []string, err error) {
+func (repo *Repo) Backup(id string, cloudInfo *CloudInfo, context map[string]interface{}) (uploadFileCount, uploadChunkCount int, uploadBytes int64, err error) {
+	repo.lock.Lock()
+	defer repo.lock.Unlock()
+
+	index, err := repo.store.GetIndex(id)
+	if nil != err {
+		return
+	}
+
+	// 从云端获取文件列表
+	cloudFileIDs, err := repo.getCloudRepoRefsFiles(cloudInfo)
+	if nil != err {
+		return
+	}
+
+	// 计算云端缺失的文件
+	var uploadFiles []*entity.File
+	for _, localFileID := range index.Files {
+		if !gulu.Str.Contains(localFileID, cloudFileIDs) {
+			var uploadFile *entity.File
+			uploadFile, err = repo.store.GetFile(localFileID)
+			if nil != err {
+				return
+			}
+			uploadFiles = append(uploadFiles, uploadFile)
+		}
+	}
+
+	// 从文件列表中得到去重后的分块列表
+	uploadChunkIDs := repo.getChunks(uploadFiles)
+
+	// 计算云端缺失的分块
+	uploadChunkIDs, err = repo.getCloudRepoUploadChunks(uploadChunkIDs, cloudInfo)
+	if nil != err {
+		return
+	}
+
+	// 上传分块
+	length, err := repo.uploadChunks(uploadChunkIDs, cloudInfo, context)
+	if nil != err {
+		return
+	}
+	uploadChunkCount = len(uploadChunkIDs)
+	uploadBytes += length
+
+	// 上传文件
+	length, err = repo.uploadFiles(uploadFiles, cloudInfo, context)
+	if nil != err {
+		return
+	}
+	uploadFileCount = len(uploadFiles)
+	uploadBytes += length
+
+	// TODO: 上传 Tags
+	return
+}
+
+func (repo *Repo) getCloudRepoUploadChunks(uploadChunkIDs []string, cloudInfo *CloudInfo) (chunks []string, err error) {
 	result := gulu.Ret.NewResult()
 	request := httpclient.NewCloudRequest(cloudInfo.ProxyURL)
 	resp, err := request.
 		SetResult(&result).
-		SetBody(map[string]string{"repo": repo, "token": cloudInfo.Token}).
+		SetBody(map[string]interface{}{"repo": cloudInfo.Dir, "token": cloudInfo.Token, "chunks": uploadChunkIDs}).
+		Post(cloudInfo.Server + "/apis/siyuan/dejavu/getRepoUploadChunks?uid=" + cloudInfo.UserID)
+	if nil != err {
+		return
+	}
+
+	if 200 != resp.StatusCode {
+		if 401 == resp.StatusCode {
+			err = ErrAuthFailed
+			return
+		}
+		err = errors.New(fmt.Sprintf("get cloud repo refs chunks failed [%d]", resp.StatusCode))
+		return
+	}
+
+	if 0 != result.Code {
+		err = errors.New(fmt.Sprintf("get cloud repo refs chunks failed: %s", result.Msg))
+		return
+	}
+
+	retData := result.Data.(map[string]interface{})
+	retChunks := retData["chunks"].([]interface{})
+	for _, retChunk := range retChunks {
+		chunks = append(chunks, retChunk.(string))
+	}
+	return
+}
+
+func (repo *Repo) getCloudRepoRefsFiles(cloudInfo *CloudInfo) (files []string, err error) {
+	result := gulu.Ret.NewResult()
+	request := httpclient.NewCloudRequest(cloudInfo.ProxyURL)
+	resp, err := request.
+		SetResult(&result).
+		SetBody(map[string]string{"repo": cloudInfo.Dir, "token": cloudInfo.Token}).
 		Post(cloudInfo.Server + "/apis/siyuan/dejavu/getRepoRefsFiles?uid=" + cloudInfo.UserID)
 	if nil != err {
 		return
@@ -55,12 +146,12 @@ func GetCloudRepoRefsFiles(repo string, cloudInfo *CloudInfo) (files []string, e
 	return
 }
 
-func GetCloudRepoTags(repo string, cloudInfo *CloudInfo) (tags []map[string]interface{}, err error) {
+func (repo *Repo) GetCloudRepoTags(cloudInfo *CloudInfo) (tags []map[string]interface{}, err error) {
 	result := gulu.Ret.NewResult()
 	request := httpclient.NewCloudRequest(cloudInfo.ProxyURL)
 	resp, err := request.
 		SetResult(&result).
-		SetBody(map[string]string{"repo": repo, "token": cloudInfo.Token}).
+		SetBody(map[string]string{"repo": cloudInfo.Dir, "token": cloudInfo.Token}).
 		Post(cloudInfo.Server + "/apis/siyuan/dejavu/getRepoTags?uid=" + cloudInfo.UserID)
 	if nil != err {
 		return
